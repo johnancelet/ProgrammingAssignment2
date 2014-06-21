c79ed3cf1360fb1ed27b41cf73809099e0de4a8d
johnancelet <johna@betterhiring.com>
2014-06-21 09:39:11 -0500

    Create makeCacheMatrix.R

---
+++ makeCacheMatrix.R
@@ -0,0 +1,16 @@
+## makeCacheMatrix takes a matrix, and compiles a special list of functions needed to run
+## cacheSolve.
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) m <<- inverse
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
